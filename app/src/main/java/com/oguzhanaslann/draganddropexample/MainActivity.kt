43a76285a9e1a94c819ce55794653f6d40fa89a9
oguzhanaslann <oguz21199@gmail.com>
2022-08-18 13:29:40 +0300

    init

---
+++ app/src/main/java/com/oguzhanaslann/draganddropexample/MainActivity.kt
@@ -0,0 +1,133 @@
+package com.oguzhanaslann.draganddropexample
+
+import android.content.ClipData
+import android.content.ClipDescription.MIMETYPE_TEXT_PLAIN
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.View.DRAG_FLAG_GLOBAL
+import android.view.View.DRAG_FLAG_GLOBAL_URI_READ
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.view.DragStartHelper
+import androidx.draganddrop.DropHelper
+import com.oguzhanaslann.draganddropexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+        DropHelper.configureView(
+            this,
+            binding.hatImageView,
+            arrayOf(MIMETYPE_TEXT_PLAIN, "image/*"),
+            DropHelper.Options.Builder()
+                .setHighlightColor(Color.GREEN)
+                .build()
+        ) { _, payload ->
+            Log.e("TAG", "onCreate: ${payload.clip.description.label} ", )
+            val isHat = binding.hatImage.tag == payload.clip.description.label
+            if (isHat) {
+                binding.hatImageView.setImageBitmap(binding.hatImage.drawable.toBitmap())
+            }
+
+            null
+        }
+
+
+        DropHelper.configureView(
+            this,
+            binding.bodyImageView,
+            arrayOf(MIMETYPE_TEXT_PLAIN, "image/*"),
+            DropHelper.Options.Builder()
+                .setHighlightColor(Color.GREEN)
+                .build()
+        ) { _, payload ->
+            Log.e("TAG", "onCreate: ${payload.clip.description.label} ", )
+            val isBody = binding.bodyImage.tag == payload.clip.description.label
+            if (isBody) {
+                binding.bodyImageView.setImageBitmap(binding.bodyImage.drawable.toBitmap())
+            }
+
+            null
+        }
+
+        DropHelper.configureView(
+            this,
+            binding.panthsImageView,
+            arrayOf(MIMETYPE_TEXT_PLAIN, "image/*"),
+            DropHelper.Options.Builder()
+                .setHighlightColor(Color.GREEN)
+                .build()
+        ) { _, payload ->
+            Log.e("TAG", "onCreate: ${payload.clip.description.label} ", )
+            val isBody = binding.panthsImage.tag == payload.clip.description.label
+            if (isBody) {
+                binding.panthsImageView.setImageBitmap(binding.panthsImage.drawable.toBitmap())
+            }
+
+            null
+        }
+
+        DragStartHelper(binding.hatImage) { view, _ ->
+            view.tag = "content://com.oguzhanaslann.draganddropexample/hatImage"
+            val data = ClipData(
+                view.tag as CharSequence,
+                arrayOf(MIMETYPE_TEXT_PLAIN),
+                ClipData.Item(view.tag as CharSequence)
+            )
+            val dragShadow = View.DragShadowBuilder(view)
+
+            view.startDragAndDrop(
+                data,
+                dragShadow,
+                null,
+                DRAG_FLAG_GLOBAL or DRAG_FLAG_GLOBAL_URI_READ
+            )
+        }.attach()
+
+        DragStartHelper(binding.bodyImage) { view, _ ->
+            view.tag = "content://com.oguzhanaslann.draganddropexample/bodyImage"
+            val data = ClipData(
+                view.tag as CharSequence,
+                arrayOf(MIMETYPE_TEXT_PLAIN),
+                ClipData.Item(view.tag as CharSequence)
+            )
+            val dragShadow = View.DragShadowBuilder(view)
+
+            view.startDragAndDrop(
+                data,
+                dragShadow,
+                null,
+                DRAG_FLAG_GLOBAL or DRAG_FLAG_GLOBAL_URI_READ
+            )
+        }.attach()
+
+        DragStartHelper(binding.panthsImage) { view, _ ->
+            // Sets the appropriate MIME types automatically.
+            view.tag = "content://com.oguzhanaslann.draganddropexample/panthsImage"
+            val data = ClipData(
+                view.tag as CharSequence,
+                arrayOf(MIMETYPE_TEXT_PLAIN),
+                ClipData.Item(view.tag as CharSequence)
+            )
+            val dragShadow = View.DragShadowBuilder(view)
+
+            view.startDragAndDrop(
+                data,
+                dragShadow,
+                null,
+                DRAG_FLAG_GLOBAL or DRAG_FLAG_GLOBAL_URI_READ
+            )
+        }.attach()
+
+    }
+}
